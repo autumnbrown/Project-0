a4a4aadcd648f1a35c31b74729be7a16815aa22f
Autumn Brown <autumnrachelbrown@gmail.com>
2021-09-03 20:43:50 -0600

    all bugs fixed?

--- project-0-main/Book.cpp
+++ project-0-main/Book.cpp
@@ -16,6 +16,7 @@ Book::Book(std::string title, std::string author) {
 
 void Book::setGenre(std::string genre) {
     this->genre = genre;
+    genreSet = true; // Bug #3 here
 }
 
 void Book::setPages(std::string pages) {
